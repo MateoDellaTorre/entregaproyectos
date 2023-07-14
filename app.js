75651c4d0046fc94a27b737b47cc084491cbfe41
Juan Della Torre <jpmateodellatorre@gmail.com>
2023-07-13 23:33:52 -0300

    cambios

---
+++ app.js
@@ -0,0 +1,11 @@
+//Declaraciones de variables
+
+
+//Tipo de dato de string
+let palabra = "Hola"
+let palabra2 = "5"
+
+let nombre = prompt("ingrese su nombre")
+
+console.log("!Hola"+ " " + nombre + "!") 
+
